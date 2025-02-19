03807b572aaa667258b1b9fa1eb7f134e9d2ef30
cerent <acerenzehra@hotmail.com>
2025-02-19 18:59:59 +0300

    first commit

---
+++ src/main/java/com/park/park_yonetim/ParkYonetimApplication.java
@@ -0,0 +1,13 @@
+package com.park.park_yonetim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkYonetimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkYonetimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/park/park_yonetim/ParkYonetimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.park_yonetim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkYonetimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
